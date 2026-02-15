0ebd5052709dbf436dc5cc479e61cb47812271b7
Keval Ambani <ambanikeval2@gmail.com>
2026-02-15 14:34:11 +0530

    Delete src/db directory

--- src/db/conn.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/JobPortal')
-.then(() => console.log('DB connected successfully'))
-.catch((err) => console.log('DB connection error:', err.message));
